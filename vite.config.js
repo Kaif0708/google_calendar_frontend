ed6b1799300e7c0bc736245c718ba75897b7cbe1
Kaif0708 <110331551+Kaif0708@users.noreply.github.com>
2025-01-20 18:36:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,18 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/auth': {
-        target: 'https://google-calendar-backend-sand.vercel.app',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/api': {
-        target: 'https://google-calendar-backend-sand.vercel.app',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/auth': {
+  //       target: 'https://google-calendar-backend-sand.vercel.app',
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //     '/api': {
+  //       target: 'https://google-calendar-backend-sand.vercel.app',
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
 });
