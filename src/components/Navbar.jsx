a396c5ec588bab61286546b73275a08b800129cc
Kaif0708 <110331551+Kaif0708@users.noreply.github.com>
2025-01-22 01:02:06 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@
 const Navbar = () => {
   return (
     // const backendUrl = process.env.REACT_APP_BACKEND_URL;
-
+//backend is called 
     <nav style={{ padding: '10px', backgroundColor: '#f8f9fa' }}>
       <a href="https://google-calendar-backend-sand.vercel.app/auth/google" style={{ marginRight: '15px' }}>Login with Google</a>
       <a href="https://google-calendar-backend-sand.vercel.app/auth/logout">Logout</a>
